8d004c3b813d2aaca7b888345d6d6f08ab21d95f
Juliana <julianamilson@hotmail.com>
2022-02-17 15:51:40 -0300

    first commit

---
+++ libs/libft/ft_lstnew.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: roaraujo <roaraujo@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/16 09:28:19 by gusalves          #+#    #+#             */
+/*   Updated: 2022/02/08 16:20:13 by roaraujo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*element;
+
+	element = (t_list *)malloc(sizeof(t_list));
+	if (element)
+	{
+		element->content = content;
+		element->next = NULL;
+	}
+	return (element);
+}
