2b4ed7b34e13c58b20c027f8702564122331064d
Juliana <julianamilson@hotmail.com>
2022-03-14 17:18:08 -0300

    Updating to find redirections

--- src/execute_process.c
+++ src/execute_process.c
@@ -2,19 +2,7 @@
 
 void	execute_process(t_root *root)
 {
-	int	cmd_size;
-	int	i;
-
-	i = 0;
-	cmd_size = ft_strlen(root->input);
-	root->command = matrix_split(root->input, cmd_size);
-	while (root->command[i])
-	{
-		root->command[i] = metacharacters_treat(root->command[i]);
-		root->command[i] = no_quotes(root->command[i]);
-		printf("%s\n", root->command[i]);
-		i++;
-	}
+	find_redirections(root);
 	root->cmd_path = what_cmd(root->command[0]);
 	if (root->cmd_path == NULL)
 	{
