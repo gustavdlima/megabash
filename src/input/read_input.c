fb0b2a342f44e21dbaf01b40bd5126dab5bf41c1
Juliana <julianamilson@hotmail.com>
2022-03-24 21:42:14 -0300

    [OK] History

--- src/input/read_input.c
+++ src/input/read_input.c
@@ -1,5 +1,54 @@
 #include "minishell.h"
 
+int	theres_delimiter(char *cmd)
+{
+	int	i;
+	int	delimiter;
+
+	i = 0;
+	delimiter = 0;
+	while (cmd[i])
+	{
+		if (cmd[i] == '<')
+			delimiter++;
+		i++;
+	}
+	if (delimiter >= 2) //pode colocar mais delimitadores?
+		return (TRUE);
+	return (FALSE);
+}
+
+int	only_space(char *cmd)
+{
+	int	i;
+	int	space;
+	int	cmd_len;
+
+	i = 0;
+	space = 0;
+	cmd_len = ft_strlen(cmd);
+	while (cmd[i])
+	{
+		if (cmd[i] == ' ')
+			space++;
+		i++;
+	}
+	if (space == cmd_len)
+		return (TRUE);
+	return (FALSE);
+}
+
+int	is_it_history(char *cmd)
+{
+	if (cmd == NULL)
+		return (FALSE);
+	if (only_space(cmd) == TRUE)
+		return (FALSE);
+	if (theres_delimiter(cmd) == TRUE)
+		return (FALSE);
+	return (TRUE);
+}
+
 char	*read_input(void)
 {
 	char	*input;
@@ -12,6 +61,7 @@ char	*read_input(void)
 	}
 	if (!ft_strncmp(input, "exit", 4))
 		exit_builtin(input);
-	add_history(input);
+	if (is_it_history(input) == TRUE)
+		add_history(input);
 	return (input);
 }
