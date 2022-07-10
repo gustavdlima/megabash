d9a6943c146f8be7f9053d5b6c9410d9cfe5f9bf
gustavdlima <gustavdlima@gmail.com>
2022-07-10 00:50:50 -0300

    export builtin 100%

--- src/builtins/export.c
+++ src/builtins/export.c
@@ -15,6 +15,13 @@ static int	print_export_error(char *name, int i)
 	return (i);
 }
 
+static void	export_new_node(char *name, char *content)
+{
+	env_addback(&g_megabash.env, env_lst_new(name, content));
+	free(name);
+	free(content);
+}
+
 static int	export_execute(char *name, char *content, char **command, int i)
 {
 	t_env	*node;
@@ -25,19 +32,13 @@ static int	export_execute(char *name, char *content, char **command, int i)
 	name = get_env_name(command[i]);
 	if (!is_alphabetic(name))
 	{
-		dprintf(2, "%s\n", name);
-		dprintf(2, "aaaaaaaa\n");
 		free(name);
 		return (print_export_error(command[i], i));
 	}
 	content = get_env_path(command[i]);
 	node = get_env_node(g_megabash.env, name);
 	if (!node)
-	{
-		env_addback(&g_megabash.env, env_lst_new(name, content));
-		free(name);
-		free(content);
-	}
+		export_new_node(name, content);
 	else
 	{
 		env_content_to_null(node, name);
@@ -60,9 +61,13 @@ void	export(char **command)
 	content = NULL;
 	while (command[i])
 	{
+		if (ft_new_strncmp("=", command[i]))
+		{
+			i = print_export_error(command[i], i);
+			continue ;
+		}
 		if ((!ft_strchr(command[i], '=')) && (!is_alphabetic(command[i])))
 		{
-			dprintf(2, "pq to caindo aqui se tenho igual? kk\n");
 			i = print_export_error(command[i], i);
 			continue ;
 		}
