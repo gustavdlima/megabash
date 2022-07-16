ffb0c9ac81779fc3f4c9a752ee61773e664337e1
Juliana <julianamilson@hotmail.com>
2022-07-16 19:31:54 -0300

    [FIXED] Open quotes

--- src/input/validate_input.c
+++ src/input/validate_input.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   validate_input.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gusalves <gusalves@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: jmilson- <jmilson-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/15 20:57:47 by gusalves          #+#    #+#             */
-/*   Updated: 2022/07/15 20:57:47 by gusalves         ###   ########.fr       */
+/*   Updated: 2022/07/16 19:01:17 by jmilson-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,28 +27,31 @@ int	theres_break_line(char *input)
 	return (false);
 }
 
-int	validate_input(char *input)
+int	validate_input(char *input, int is_valid)
 {
-	treat_input_chars(input);
-	if (input)
+	if (is_valid)
 	{
-		if (!input || only_space(input) == true)
-			return (false);
-		else if (bash_syntax_error(input) == true)
-			return (false);
-		else if (different_redirection_signs(input) == true)
-			return (false);
-		else if (too_many_redirections(input) == true)
-			return (false);
-		else if (redirect_to_no_arguments(input) == true)
-			return (false);
-		else if (no_words_after_redirect(input) == true)
-			return (false);
-		else if (pipe_no_arguments(input) == true)
-			return (false);
-		else if (theres_break_line(input) == true)
-			return (false);
-		return (true);
+		treat_input_chars(input);
+		if (input)
+		{
+			if (!input || only_space(input) == true)
+				return (false);
+			else if (bash_syntax_error(input) == true)
+				return (false);
+			else if (different_redirection_signs(input) == true)
+				return (false);
+			else if (too_many_redirections(input) == true)
+				return (false);
+			else if (redirect_to_no_arguments(input) == true)
+				return (false);
+			else if (no_words_after_redirect(input) == true)
+				return (false);
+			else if (pipe_no_arguments(input) == true)
+				return (false);
+			else if (theres_break_line(input) == true)
+				return (false);
+			return (true);
+		}
 	}
 	return (false);
 }
